3c86e8cefbdd89d7a395faf6daf87a23329a5af7
williamgundim <williammatos09@gmail.com>
2020-08-17 15:16:40 -0300

    criado metodo get para a pagina 3 de avaliacao

--- controllers/avlc-three.js
+++ controllers/avlc-three.js
@@ -5,6 +5,15 @@ module.exports = app => {
     /**
      * Rota para cadastrar/retornar os valores da terceira pagina da avaliação.
      */
+
+    app.get('/avlcthree/id', (request, response)=> {
+
+        const idPaciente = request.params.id;
+
+        model.getItems(idPaciente, response);
+
+    });
+
     app.post('/avlcthree', (req, res) => {
         
         const value = req.body;
@@ -24,10 +33,5 @@ module.exports = app => {
 
     });
 
-    app.get('/avlcthree', (request, response)=> {
-
-        model.getItems(response);
-
-    });
 
 } 
--- models/model-avlc-three.js
+++ models/model-avlc-three.js
@@ -35,6 +35,41 @@ class ModelAvlcThree{
 
     }
 
+
+/**
+     * getItems()
+     */
+    getItems(idPaciente, response){
+
+        const sql = `SELECT ID_DOENCA FROM AVLC_THREE WHERE ID_PATIENT = ${idPaciente}`
+
+        connection.query(sql, (error, result) => {
+
+            let oJson = {};
+            let aList = [];
+            let x = 0;
+
+            if (error){
+                response.status(400).json(error) //bad request.
+            }else{
+                
+                for (x = 0; x < result.length; x++){                 
+                    aList.push( result[x].ID_DOENCA );
+                }
+
+                oJson = {
+                    'IDPACIENTE':idPaciente,
+                    'LISTID': aList,
+                }
+
+                response.status(200).json(oJson);
+            }
+
+
+        })
+
+    }
+
 }
 
 module.exports = new ModelAvlcThree;
